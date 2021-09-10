11ed5f64d0a94c91a134add00eb0ada96d9164ca
Nicolas <nicolas.tan@le-campus-numerique.fr>
2021-09-10 21:57:54 +0200

    class JWT created

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'includes/config.php';
+require_once 'classes/JWT.php';
 // on crée le header
 // chaine de caractère est un encodage de notre header quand on est sur le site https://jwt.io/
 $header = [
@@ -19,44 +20,9 @@ $payload = [
     ]
 ];
 
-// on transforme le header et payload en chaine de caractères comme à gauche sur le site, on dit que :
-// on encode le header/payload en base64
-// on utilise json_encode() car $header est un tableau
-$base64Header = base64_encode(json_encode($header));
-$base64Payload = base64_encode(json_encode($payload));
+$jwt = new JWT();
+$token = $jwt->generate($header, $payload, SECRET);
 
-// echo $base64Header;
-// echo $base64Payload;
-
-// on "nettoie" les valeurs encodées
-// on retire les +, / et =
-// sinon erreur "Invalid Signature" à gauche
-$base64Header = str_replace(['+', '/', '='], ['-', '_', ''], $base64Header);
-$base64Payload = str_replace(['+', '/', '='], ['-', '_', ''], $base64Payload);
-
-// la 3e partie vérifie que les 2 premières ont bien été générés par mon serveur, sinon l'authentification se fait quand même et le token ne sert plus a rien ; la 3e partie vérifie l'authenticité du token
-
-$secret = base64_encode(SECRET); 
-// c'est le "secret base64 encoded" à droite du site, qu'on ne doit jamais communiquer/se faire voler
-// echo $secret;
-
-// on génère la signature en lui passant $base64Header et $base64Payload
-// hash_hmac() fonction de génération de hash
-// sha256 pour générer ce hash
-$signature = hash_hmac('sha256', $base64Header . '.' . $base64Payload, $secret, true);
-
-$signature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-
-// echo $signature;
-
-// ou pour voir l'étape intermédiaire juste avant la ligne précédente :
-// $base64Signature = base64_encode($signature);
-// $signature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-// echo $base64Signature;
-
-// on crée le token
-$jwt = $base64Header . '.' . $base64Payload . '.' . $signature;
-
-echo $jwt;
+echo $token;
 
 
