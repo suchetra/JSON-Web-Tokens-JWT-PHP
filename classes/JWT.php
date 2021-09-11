dce96ed66ae7318f139b932fbb452a4e0a02d52f
Nicolas <nicolas.tan@le-campus-numerique.fr>
2021-09-11 11:24:33 +0200

    copy past

--- classes/JWT.php
+++ classes/JWT.php
@@ -1,9 +1,16 @@
 <?php
 class JWT
 {
+    /**
+     * Génération JWT
+     * @param array $header Header du token
+     * @param array $payload Payload du Token
+     * @param string $secret Clé secrète
+     * @param int $validity Durée de validité (en secondes)
+     * @return string Token
+     */
     public function generate(array $header, array $payload, string $secret, int $validity = 86400): string
     {
-
         if($validity > 0){
             $now = new DateTime();
             $expiration = $now->getTimestamp() + $validity;
@@ -11,81 +18,104 @@ class JWT
             $payload['exp'] = $expiration;
         }
 
-        // on transforme le header et payload en chaine de caractères comme à gauche sur le site, on dit que :
-        // on encode le header/payload en base64
-        // on utilise json_encode() car $header est un tableau
+        // On encode en base64
         $base64Header = base64_encode(json_encode($header));
         $base64Payload = base64_encode(json_encode($payload));
 
-        // echo $base64Header;
-        // echo $base64Payload;
-
-        // on "nettoie" les valeurs encodées
-        // on retire les +, / et =
-        // sinon erreur "Invalid Signature" à gauche
+        // On "nettoie" les valeurs encodées
+        // On retire les +, / et =
         $base64Header = str_replace(['+', '/', '='], ['-', '_', ''], $base64Header);
         $base64Payload = str_replace(['+', '/', '='], ['-', '_', ''], $base64Payload);
 
-        // la 3e partie vérifie que les 2 premières ont bien été générés par mon serveur, sinon l'authentification se fait quand même et le token ne sert plus a rien ; la 3e partie vérifie l'authenticité du token
+        // On génère la signature
+        $secret = base64_encode(SECRET);
 
-        $secret = base64_encode(SECRET); 
-        // c'est le "secret base64 encoded" à droite du site, qu'on ne doit jamais communiquer/se faire voler
-        // echo $secret;
-
-        // on génère la signature en lui passant $base64Header et $base64Payload
-        // hash_hmac() fonction de génération de hash
-        // sha256 pour générer ce hash
         $signature = hash_hmac('sha256', $base64Header . '.' . $base64Payload, $secret, true);
 
-        $signature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+        $base64Signature = base64_encode($signature);
 
-        // echo $signature;
+        $signature = str_replace(['+', '/', '='], ['-', '_', ''], $base64Signature);
 
-        // ou pour voir l'étape intermédiaire juste avant la ligne précédente :
-        // $base64Signature = base64_encode($signature);
-        // $signature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-        // echo $base64Signature;
-
-        // on crée le token
+        // On crée le token
         $jwt = $base64Header . '.' . $base64Payload . '.' . $signature;
 
         return $jwt;
     }
 
+    /**
+     * Vérification du token
+     * @param string $token Token à vérifier
+     * @param string $secret Clé secrète
+     * @return bool Vérifié ou non
+     */
     public function check(string $token, string $secret): bool
     {
-        // on récupère le header
+        // On récupère le header et le payload
         $header = $this->getHeader($token);
         $payload = $this->getPayload($token);
 
-        // on génère un token de vérification
-        // 0 car on ne veut pas ajouter des dates à la fonction generate qui en a déjà... sinon le token sera différent
+        // On génère un token de vérification
         $verifToken = $this->generate($header, $payload, $secret, 0);
 
         return $token === $verifToken;
     }
 
+    /**
+     * Récupère le header
+     * @param string $token Token
+     * @return array Header
+     */
     public function getHeader(string $token)
     {
-        // démontage token
+        // Démontage token
         $array = explode('.', $token);
 
-        // on décode le header
+        // On décode le header
         $header = json_decode(base64_decode($array[0]), true);
 
         return $header;
-
     }
 
+    /**
+     * Retourne le payload
+     * @param string $token Token
+     * @return array Payload
+     */
     public function getPayload(string $token)
     {
-        // démontage token
+        // Démontage token
         $array = explode('.', $token);
 
-        // on décode le header
+        // On décode le payload
         $payload = json_decode(base64_decode($array[1]), true);
 
         return $payload;
     }
 
+    /**
+     * Vérification de l'expiration
+     * @param string $token Token à vérifier
+     * @return bool Vérifié ou non
+     */
+    public function isExpired(string $token): bool
+    {
+        $payload = $this->getPayload($token);
+
+        $now = new DateTime();
+
+        return $payload['exp'] < $now->getTimestamp();
+    }
+
+    /**
+     * Vérification de la validité du token
+     * @param string $token Token à vérifier
+     * @return bool Vérifié ou non
+     */
+    public function isValid(string $token): bool
+    {
+        return preg_match(
+            '/^[a-zA-Z0-9\-\_\=]+\.[a-zA-Z0-9\-\_\=]+\.[a-zA-Z0-9\-\_\=]+$/',
+            $token
+        ) === 1;
+    }
 }
--- decode.php
+++ decode.php
@@ -1,11 +1,8 @@
 <?php
-const TOKEN = 'eyJ0eXAiOiJqV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjMsInJvbGVzIjpbIlJPTEVfQURNSU4iLCJST0xFX1VTRVIiXSwiZW1haWwiOiJjb250YWN0QGRlbW8uZnIiLCJpYXQiOjE2MzEzNDE3NTMsImV4cCI6MTYzMTQyODE1M30.Or0XHNE2rEonNoVEMW8pEEhORDgXK2mOXtiy_aIMCeE';
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjMsInJvbGVzIjpbIlJPTEVfQURNSU4iLCJST0xFX1VTRVIiXSwiZW1haWwiOiJjb250YWN0QGRlbW8uZnIiLCJpYXQiOjE2MjM1MDIxMzgsImV4cCI6MTYyMzUwMjE5OH0.fk5_pPeO-gITVsRk9Aijv8h3upq32e3cnpFGCpyi1eY';
 
 require_once 'includes/config.php';
 require_once 'classes/JWT.php';
 
 $jwt = new JWT();
-// var_dump($jwt->getHeader(TOKEN));
-// var_dump($jwt->getPayload(TOKEN));
-
-var_dump($jwt->check(TOKEN, SECRET));
+var_dump($jwt->isExpired(TOKEN));
